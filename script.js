72d20accbee79c5ba44801d6404d061e6281ebb1
mbool <mstybowski@gmail.com>
2021-05-30 18:23:26 +0200

    circles, 3 phases

--- script.js
+++ script.js
@@ -1,64 +1,45 @@
-var alphabet = "abcdefghijklmnoprstuwyz".split('');
-
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height"),
-    g = svg.append("g").attr("transform", "translate(32," + (height / 2) + ")");
-
-function update(_data) {
-  const data = ['a', ..._data];
-  var t = d3.transition()
-      .duration(750);
-// console.log(data)
-  // JOIN new data with old elements.
-  var text = g.selectAll("text")
-    .data(data, function(d) {console.log(this); return d});
-
-  
-  console.log(text)
-  
-  // EXIT old elements not present in new data.
-  text.exit()
-      .attr("class", "exit")
-    .transition(t)
-      .attr("y", 60)
-      .style("fill-opacity", 1e-6)
-      .remove();
-
-  // UPDATE old elements present in new data.
-  text.attr("class", "update")
-      .attr("y", 0)
-      .style("fill-opacity", 1)
-    .transition(t)
-      .attr("x", function(d, i) { return i * 32; });
-
-  
-  // ENTER new elements present in new data.
-  text.enter().append("text")
-      .attr("class", "enter")
-      .attr("dy", ".35em")
-      .attr("y", -60)
-      .attr("x", function(d, i) { return i * 32; })
-      .style("fill-opacity", 1e-6)
-      .text(function(d) { return d; })
-    .transition(t)
-      .attr("y", 0)
-      .style("fill-opacity", 1);
+const svgCanvas = d3.select("svg").append("g");
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// The initial display.
-update(alphabet);
+const update = (data) => {
 
-// Grab a random sample of letters from the alphabet, in alphabetical order.
-const myi = setInterval(function() {
-  update(d3.shuffle(alphabet)
-      .slice(0, Math.floor(Math.random() * 24)+1))
-}, 8500);
+let datedGroup = svgCanvas.selectAll("circle").data(data);
 
-function stop(){
-  clearInterval(myi)
+datedGroup
+  .enter()
+  .append("circle")
+  .attr("r", 0)
+  .attr("cx", (d, i) => i * 30+40)
+  .attr("cy", 0);
+
+datedGroup
+  .transition()
+  .duration(300)
+  .attr("r", (d) => d)
+  .attr("cx", (d, i) => i * 30 + 40)
+  .attr("cy", 50);
+
+datedGroup.exit().transition().duration(300).attr('r', 0)
+
 }
 
+const getNewData = (function(){
+  let data = new Array(30).fill().map(() => getRandomInt(1,15));
+  update(data);
+  return () => {
+    const indexToRemove =  getRandomInt(0, data.length);
+    console.log('Index to remove: ' + indexToRemove);
+      data.splice(indexToRemove, 1);
+      return data;
+  }
+})();
 
+const removeRandom = () => {
 
+  update(getNewData())
 
+}
